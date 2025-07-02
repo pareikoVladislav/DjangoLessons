0ec46a5dba126682a7198d351249971b312752e9
vPareiko <parieiko.vlad@gmail.com>
2025-07-02 13:52:42 +0400

    feat: Work with models && field's parameters

--- src/library/models/__init__.py
+++ src/library/models/__init__.py
@@ -1,4 +1,5 @@
 from src.library.models.book import Book
+from src.library.models.soc_media import Post, UserProfile, Comment
 
 
-__all__ = ["Book"]
+__all__ = ["Book", "Post", "UserProfile", "Comment"]
