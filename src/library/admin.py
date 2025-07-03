f2420ed661036181619dd1024470e0cb20896f63
AlexeyShchukin <shchukin.developer@gmail.com>
2025-07-03 11:18:22 +0200

    feat: Add model publisher and relation to book

--- src/library/admin.py
+++ src/library/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from src.library.models import Book, Post, UserProfile, Comment, Author
+from src.library.models import Book, Post, UserProfile, Comment, Author, Publisher
 
 admin.site.register(Book)
 admin.site.register(Post)
 admin.site.register(UserProfile)
 admin.site.register(Comment)
 admin.site.register(Author)
+admin.site.register(Publisher)
