b5534fe839640df495ebdf4b2f7188afa2312f11
antonshk2023 <antonshk2013@gmail.com>
2025-07-07 10:13:20 +0200

    feat: add AuthorAdmin actions

--- src/library/admin/author.py
+++ src/library/admin/author.py
@@ -20,3 +20,28 @@ class AuthorAdmin(admin.ModelAdmin):
             }
         )
     )
+    actions = ['deactivate_authors', 'activate_authors', 'export_to_json']
+
+    @admin.action(description="Деактивировать выбранных авторов")
+    def deactivate_authors(modeladmin, request, queryset):
+        queryset.update(is_active=False)
+        return request
+    @admin.action(description="Активировать выбранных авторов")
+    def activate_authors(modeladmin, request, queryset):
+        queryset.update(is_active=True)
+        return request
+
+    @admin.action(description="Экспорт в JSON")
+    def export_to_json(self, request, queryset):
+        import json
+        from django.http import HttpResponse
+
+        data = list(queryset.values())
+        for item in data:
+            for key, value in item.items():
+                if hasattr(value, 'isoformat'):
+                    item[key] = value.isoformat()
+        file_data = json.dumps(data)
+        response = HttpResponse(file_data, content_type='application/json')
+        response['Content-Disposition'] = 'attachment; filename=author.json'
+        return response
