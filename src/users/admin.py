0290a8d551e42717ae36eb3e86b589afbc296c91
vPareiko <parieiko.vlad@gmail.com>
2025-07-09 00:09:29 +0400

    fix: Update migrations && add new Users app

--- src/users/admin.py
+++ src/users/admin.py
@@ -12,7 +12,7 @@ class CustomUserAdmin(BaseUserAdmin):
         (None, {'fields': ('username', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'birth_date')}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        (_('Important dates'), {'fields': ('last_login',)}),
     )
     add_fieldsets = (
         (None, {
