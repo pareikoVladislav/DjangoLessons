92aa083a3d67bf4a15850e92bbc1bb284442fb50
vPareiko <parieiko.vlad@gmail.com>
2025-07-23 17:33:30 +0400

    feat: Generics 2

--- src/users/views.py
+++ src/users/views.py
@@ -6,6 +6,8 @@ from rest_framework.generics import (
     ListAPIView,
     ListCreateAPIView, RetrieveUpdateDestroyAPIView
 )
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 from src.users.service import UserService
 from src.users.dtos import ListUsersDTO, DetailedUserDTO, CreateUserDTO
@@ -78,16 +80,37 @@ from src.users.models import User
 class UsersListAPIView(ListCreateAPIView):
     # serializer_class = ListUsersDTO
     queryset = User.objects.all()
+    filter_backends = [
+        DjangoFilterBackend,
+        SearchFilter,
+        OrderingFilter
+    ]
+    filterset_fields = ['last_name', 'gender', 'role']
+    search_fields = ['username', 'email']
+    ordering_fields = ['age', 'is_staff', 'date_joined']
+
 
     def get_serializer_class(self):
         if self.request.method == 'GET':
             return ListUsersDTO
         return CreateUserDTO
 
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+
+        context['include_related'] = self.request.query_params.get(
+            'include_related',
+            'false'
+        ).lower() == 'true'
+
+        return context
+
 
 class UserRetrieveAPIView(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
-    lookup_url_kwarg = 'user_id'
+    # lookup_field = 'username'
+    lookup_url_kwarg = 'username'
+    # username = url dynamic param <>
 
     def get_serializer_class(self):
         if self.request.method == 'GET':
