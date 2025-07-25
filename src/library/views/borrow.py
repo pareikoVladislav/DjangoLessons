80fc20cb84b1e370846b59661b8407e3d30d958d
beper223 <zashalovskiy@gmail.com>
2025-07-25 15:23:15 +0200

    top borrower task 26

--- src/library/views/borrow.py
+++ src/library/views/borrow.py
@@ -3,9 +3,11 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
+from django.db.models import Count
 
-from src.library.dtos.borrow import BorrowDTO, OverdueBorrowsDTO
+from src.library.dtos.borrow import BorrowDTO, OverdueBorrowsDTO, TopBorrowerSerializer
 from src.library.models import Borrow
+from src.users.models import User
 
 
 class BorrowViewSet(ModelViewSet):
@@ -24,3 +26,19 @@ class BorrowViewSet(ModelViewSet):
             data=serializer,
             status=status.HTTP_200_OK
         )
+
+    @action(methods=["get"], detail=False, url_path="top-borrowers")
+    def get_top_borrower(self, request):
+        top_user = (
+            User.objects
+            .annotate(books_borrowed=Count('borrows__borrows'))
+            .order_by('-books_borrowed')
+            .filter(books_borrowed__gt=0)
+            [:5]
+        )
+
+        if not top_user:
+            return Response([], status=status.HTTP_200_OK)
+
+        serializer = TopBorrowerSerializer(top_user,many=True)
+        return Response([serializer.data], status=status.HTTP_200_OK)
