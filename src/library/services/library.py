40cdcda8b1cdee2041ac456ebe6b7b3c220588f4
AlexeyShchukin <shchukin.developer@gmail.com>
2025-07-31 22:03:01 +0200

    feat: Add create library members view

--- src/library/services/library.py
+++ src/library/services/library.py
@@ -1,12 +1,19 @@
-from django.db import DatabaseError
+from typing import Any
+
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import DatabaseError, transaction
 
 from src.library.dtos.statistics import LibraryStatisticDTO
 from src.library.repositories.library import LibraryRepository
+from src.library.repositories.library_record import LibraryRecordRepository
+from src.users.repository import UserRepository
 
 
 class LibraryService:
     def __init__(self):
         self.library_repo = LibraryRepository()
+        self.user_repo = UserRepository()
+        self.library_record_repo = LibraryRecordRepository()
 
     def get_statistic_by_library(self, query_params):
         try:
@@ -16,7 +23,7 @@ class LibraryService:
                 "count_books_per_genre": self.library_repo.count_books_per_genre(),
                 "top_genres": self.library_repo.get_top_genres_per_library(query_params),
                 "count_readers": self.library_repo.count_readers(),
-                "top_readers":  self.library_repo.get_top_readers(),
+                "top_readers": self.library_repo.get_top_readers(),
                 "top_active_weekdays": self.library_repo.get_most_active_weekdays()
             }
             serializer = LibraryStatisticDTO(statistic).data
@@ -24,3 +31,34 @@ class LibraryService:
 
         except DatabaseError as e:
             return f"Database Error: {str(e)}"
+
+    def create_new_members(
+            self,
+            library_id: int,
+            user_data_list: list[dict[str, Any]]
+    ) -> list:
+        try:
+            transaction.set_autocommit(False)
+
+            library = self.library_repo.get_by_id(library_id)
+            if not library:
+                raise ObjectDoesNotExist(f"Library with id {library_id} not found.")
+
+            created_users = []
+            for user_data in user_data_list:
+                user = self.user_repo.create(**user_data)
+                self.library_record_repo.create(
+                    library_id=library.id,
+                    member_id=user.id
+                )
+                created_users.append(user)
+
+            transaction.commit()
+            return created_users
+
+        except Exception as e:
+            transaction.rollback()
+            raise
+
+        finally:
+            transaction.set_autocommit(True)
