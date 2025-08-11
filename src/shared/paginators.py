b5a2c63c73dda188a14bf352810d2dfaba257686
Mihail <kanevskymihail@gmail.com>
2025-08-11 11:48:11 +0200

    feat/categories_permissions-50

--- src/shared/paginators.py
+++ src/shared/paginators.py
@@ -3,7 +3,9 @@ from rest_framework.pagination import CursorPagination, PageNumberPagination
 
 class CustomPageNumber(PageNumberPagination):
     page_size = 10
-    page_query_param = 'page_size'
+    page_query_param = 'page'
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 
 class CustomCursorPaginator(CursorPagination):
