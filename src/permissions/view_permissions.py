8a99e1edac82eef90e8eb27b703e928a9f24a64f
antonshk2013 <anton.shkarupa@gmail.com>
2025-08-11 09:56:14 +0200

    feat:permisions for actioons list and create BorrowViewSet, refactor:srs.permissions to paccagefeat(borrow): add permissions for list and create actions in BorrowViewSet refactor(permissions): move permissions to package

---
+++ src/permissions/view_permissions.py
@@ -0,0 +1,14 @@
+from django.utils import timezone
+from rest_framework.permissions import (
+    BasePermission,
+)
+
+class CanGetTopBorrower(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.has_perm('library.can_get_top_borrower')
+
+
+class IsWorkHour(BasePermission):
+    def has_permission(self, request, view):
+        current = timezone.localtime().hour + 4
+        return 8 <= current < 18
