d5aa639b7b9c739074f885e29ca2eaec0b0ceec8
vPareiko <parieiko.vlad@gmail.com>
2025-08-11 13:47:39 +0400

    feat: add login endpoint

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from src.users.views import UsersListAPIView, UserRetrieveAPIView
+from src.users.views import (
+    UsersListAPIView,
+    UserRetrieveAPIView,
+    LoginUserAPIView
+)
 
 urlpatterns = [
     path('', UsersListAPIView.as_view()),
+    path('auth-login/', LoginUserAPIView.as_view()),
     path('<str:username>/', UserRetrieveAPIView.as_view()),
 ]
--- src/users/views.py
+++ src/users/views.py
@@ -1,10 +1,19 @@
-from rest_framework.permissions import IsAuthenticated
+from datetime import datetime
+
+from django.contrib.auth import authenticate
+from django.utils.timezone import make_aware
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.generics import (
     ListCreateAPIView,
     RetrieveUpdateDestroyAPIView
 )
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from src.users.dtos import ListUsersDTO, DetailedUserDTO, CreateUserDTO
 from src.users.models import User
@@ -47,3 +56,65 @@ class UserRetrieveAPIView(RetrieveUpdateDestroyAPIView):
             return DetailedUserDTO
         return CreateUserDTO
 
+
+class LoginUserAPIView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request: Request, *args, **kwargs) -> Response:
+        login, password = request.data.get('username'), request.data.get('password')
+
+        try:
+            user = authenticate(
+                request=request,
+                username=login,
+                password=password
+            )
+
+            if user:
+                response = Response(status=status.HTTP_200_OK)
+
+
+                refresh = RefreshToken.for_user(user)
+                access = refresh.access_token
+
+                refresh_exp = make_aware(
+                    datetime.fromtimestamp(refresh.payload['exp'])
+                )
+                access_exp = make_aware(
+                    datetime.fromtimestamp(access.payload['exp'])
+                )
+
+                response.set_cookie(
+                    key='refresh',
+                    value=str(refresh),
+                    httponly=True,
+                    secure=True,
+                    samesite='Lax',
+                    expires=refresh_exp
+                )
+
+                response.set_cookie(
+                    key='access',
+                    value=str(access),
+                    httponly=True,
+                    secure=True,
+                    samesite='Lax',
+                    expires=access_exp
+                )
+
+                return response
+
+            else:
+                return Response(
+                    data={
+                        'error': 'Invalid username or password'
+                    },
+                    status=status.HTTP_401_UNAUTHORIZED
+                )
+        except Exception as e:
+            return Response(
+                data={
+                    'error': str(e)
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
