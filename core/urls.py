36af55ec12288d8eb8be383ecf7314fc2d62d619
vPareiko <parieiko.vlad@gmail.com>
2025-08-19 19:06:17 +0400

    fix: test CI

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -31,4 +33,4 @@ urlpatterns = [
     ),
 
     path('api/v1/', include('src.routes')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
